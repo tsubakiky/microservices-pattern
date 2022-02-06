e760d77d70dd392b08586581fc38dd5358560d39
tsubaki <salovers1205@gmail.com>
2022-02-06 20:47:48 +0900

    fix: 🐛 init upgrade ~> 3.53

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -9,7 +9,12 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.9.0"
+      version = "~> 3.53"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.53"
     }
   }
+  required_version = ">= 0.13"
 }
