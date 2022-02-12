86df1247cc9fc531d9fc6cb7d7c48194be415c81
tsubaki <salovers1205@gmail.com>
2022-02-12 18:39:50 +0900

    fix: 🐛 noauth

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,10 +84,20 @@ resource "google_cloud_run_service" "default" {
   }
 }
 
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
 resource "google_cloud_run_service_iam_member" "public-access" {
   location = google_cloud_run_service.default.location
   project  = google_cloud_run_service.default.project
   service  = google_cloud_run_service.default.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+
 }
